1a7beb549da914852fe7aa7faaec63d8f5c58aab
rinick <rinick@gmail.com>
2026-08-23 17:52:28 -0700

    [node-8] Decode top-level RegExp classes as WTF-8
    Node-8-V8-Commit: 272c8085e7a51b47c965246dd3160c83a7983d54

--- deps/v8/src/heap/factory.cc
+++ deps/v8/src/heap/factory.cc
@@ -4470,6 +4470,7 @@ DirectHandle<RegExpData> Factory::NewIrRegExpData(DirectHandle<String> source,
   instance->clear_uc16_code();
   instance->clear_latin1_bytecode();
   instance->clear_uc16_bytecode();
+  instance->clear_node8_class_ranges();
   instance->set_capture_name_map(Smi::FromInt(JSRegExp::kUninitializedValue));
   instance->set_max_register_count(JSRegExp::kUninitializedValue);
   instance->set_capture_count(capture_count);
@@ -4508,6 +4509,7 @@ DirectHandle<RegExpData> Factory::NewExperimentalRegExpData(
   instance->clear_uc16_code();
   instance->clear_latin1_bytecode();
   instance->clear_uc16_bytecode();
+  instance->clear_node8_class_ranges();
   instance->set_capture_name_map(Smi::FromInt(JSRegExp::kUninitializedValue));
   instance->set_max_register_count(JSRegExp::kUninitializedValue);
   instance->set_capture_count(capture_count);
